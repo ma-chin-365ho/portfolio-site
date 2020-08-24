4984ca21f6f10db914f4540facd884a81ed45a6f
mamemamema <st.msnr.wk@gmail.com>
2020-08-24 23:05:25 +0900

    simple ver 2

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
 const http = require('http');
  
-const hostname = 'www.ma299.me';
-const port = 80;
+// const hostname = 'www.ma299.me';
+const hostname = '127.0.0.1'
+// const port = 80;
+const port = 3000;
  
+var fs = require('fs');
+
+/***************************************************************/
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+    fs.readFile('./public/index.html', 'utf-8' , doReard );    
+    function doReard(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    }
 });
+
  
+/*
+server.on('request', doRequest);
+ 
+function doRequest(req, res) {
+    
+    fs.readFile('./public/index.html', 'utf-8' , doReard );    
+    function doReard(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    }
+    
+}
+*/
+/***************************************************************/
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
